a68467bcac8ec39e27a141855ad047157d5b16e5
tnbrowncontam <49096119+tnbrowncontam@users.noreply.github.com>
2021-12-13 13:19:32 -0400

    Added uncertainty estimate for HLbiodeg

--- models/meta_qsar_hlbiodeg.py
+++ models/meta_qsar_hlbiodeg.py
@@ -11,11 +11,11 @@ smiles_flag = 'neutrals'
 
 
 def calculate(solutedependencies, solventdependencies):
-    domainnotes = []
+    domainnotes = ['generic error estimated from uncertainties of model fits']
     # estimate HLbiodeg from both biowin survey models and take the average
     biowin3 = solutedependencies['biowin3usmmlrx'][0] + solutedependencies['biowin3usmmlra'][0]
     biowin4 = solutedependencies['biowin4psmmlrx'][0] + solutedependencies['biowin4psmmlra'][0]
     HLbiodeg = ((biowin3 * -1.07 + 4.2) + (biowin4 * -1.46 + 6.51)) / 2
 
-    return round(HLbiodeg, round_digits), np.nan, round(0, round_digits), ', '.join(domainnotes)
+    return round(HLbiodeg, round_digits), np.nan, round(1.11, round_digits), ', '.join(domainnotes)
 
